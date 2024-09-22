1eb846541f77ae47656b7727abbc8a9fe63e1448
TheSoftNode <thesoftnode@gmail.com>
2024-09-22 08:50:18 +0100

    Add the email module to the app module

--- auth_server/src/app.module.ts
+++ auth_server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
@@ -16,6 +17,8 @@ import { ConfigModule } from '@nestjs/config';
         federation: 2,
       },
     }),
+
+    EmailModule,
   ],
   controllers: [],
   providers: [
