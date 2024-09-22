ea7382149fef84be58e02feccbb0af0c7bf87a86
TheSoftNode <thesoftnode@gmail.com>
2024-09-22 08:54:23 +0100

    Configured the email module

--- auth_server/src/email/email.module.ts
+++ auth_server/src/email/email.module.ts
@@ -1,9 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-import { EmailController } from './email.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
+@Global()
 @Module({
-  controllers: [EmailController],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('SMTP_HOST'),
+          secure: true,
+          auth: {
+            user: config.get('SMTP_MAIL'),
+            pass: config.get('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: 'RestaurantApp',
+        },
+        template: {
+          dir: join(__dirname, '../../../apps/users/email-templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [EmailService],
+  exports: [EmailService],
 })
-export class EmailModule {}
+export class EmailModule { }
