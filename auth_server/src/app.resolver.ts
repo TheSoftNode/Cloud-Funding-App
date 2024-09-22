7cce64f623361b77cb96062c59549a40a59f2be3
TheSoftNode <thesoftnode@gmail.com>
2024-09-22 09:05:34 +0100

    Add the register resolver endpoint

--- auth_server/src/app.resolver.ts
+++ auth_server/src/app.resolver.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { RegisterResponse } from './types/user.types';
+import { RegisterDto } from './dtos/user.dto';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Resolver('User')
+export class AppResolver {
+  constructor(private readonly userService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Mutation(() => RegisterResponse)
+    async register(
+        @Args('registerDto') registerDto: RegisterDto,
+        @Context() context: { res: Response },
+    ): Promise<RegisterResponse>
+    {
+        if (!registerDto.name || !registerDto.email || !registerDto.password)
+        {
+            throw new BadRequestException('Please fill the all fields');
+        }
+
+        const { activation_token, activationCode } = await this.userService.register(
+            registerDto,
+            context.res,
+        );
+
+        return { activation_token, activationCode };
+    }
 }
